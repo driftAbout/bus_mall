ffc34d34edd51d31afe7eb8b75f822fdaf3c9dca
Kevin MIller <kevin@driftabout.com>
2017-11-01 02:15:58 -0700

    cleaned up

--- js/app.js
+++ js/app.js
@@ -17,16 +17,11 @@ var imageCount = imageNames.length;
 var selections = document.getElementById('selections');
 //target element for loaded images
 var image_display = document.getElementById('image_display');
-//section containing canvas
-var results_container = document.getElementById('results');
-//targert element for chart
-var canvas = document.getElementById('results_chart');
-var ctx = canvas.getContext('2d');
-var chart;
+
 
 var welcome = document.getElementById('welcome');
 var start = document.getElementById('start');
-
+var image_div = document.getElementById('small_image');
 
 // array to hold unique index numbers
 var unique_Nums;
@@ -34,12 +29,33 @@ var unique_Nums;
 var previous_unique = [];
 // session counter
 var roundCount = 0;
-var totalRounds = 3;
+var totalRounds = 25;
 
 //time delay between choices
-var timeDelay = 500;
-var delayedTime;
+var timeDelay = 750;
 var targetImages;
+
+//**********chart stuff *****************///
+//section containing canvas
+var results_container = document.getElementById('results');
+//targert element for chart
+var canvas = document.getElementById('results_chart');
+var ctx = canvas.getContext('2d');
+var chart;
+var chartType = 'bar';
+var percent_background_color = '#C3B867';
+var percent_border_color = '#000000';
+var views_background_color = '#767254';
+var views_border_color = '#767254';
+var clicked_background_color = '#F7D50A';
+var clicked_border_color = '#000000';
+var roundsAxes_labelString = 'Number of Selection Rounds';
+var percentAxes_labelString = 'Percentage of Clicks / View';
+var yAxes_fontSize = 18;
+
+//********* start *******//
+welcome_start();
+console.log('allProducts', allProducts);
 
 
 /*****************************************/
@@ -55,8 +71,6 @@ function Product(fileName){
   this.views = 0;
   this.clicks = 0;
   this.percentSelected = 0;
-  this.tallyString = 'votes for the';
-  this.dataRow;
   this.createImageInfo();
 };
 
@@ -64,40 +78,37 @@ Product.prototype.createImageInfo = function(){
   this.imagePath = this.imageFolder + '/' + this.fileName;
   var nameParts = this.fileName.split('.');
   this.productName = nameParts[0];
-  this.createDataRow();
 };
 
 Product.prototype.viewCounter = function(){
   this.views++;
-  this.percentSelected = Math.round((this.clicks / this.views) * 1000) / 10;
-  this.createDataRow();
+  this.calulatePercentage();
+  //this.percentSelected = Math.round((this.clicks / this.views) * 1000) / 10;
 };
 
 Product.prototype.clickCounter = function(){
   this.clicks++;
-  this.percentSelected = Math.round((this.clicks / this.views) * 1000) / 10;
-  this.createDataRow();
+  this.calulatePercentage();
+  //this.percentSelected = Math.round((this.clicks / this.views) * 1000) / 10;
 };
 
-Product.prototype.createDataRow = function(){
-  var dataRow = document.createElement('tr');
-  var dataArray = [this.clicks, this.tallyString, this.productName];
-  var dataString = '<tr><td>' + dataArray.join('</tr><td>') + '</tr></td>';
-  dataRow.innerHTML = dataString;
-  this.dataRow = dataRow;
+Product.prototype.calulatePercentage = function(){
+  //calculate percentage to 1 decimal place
+  this.percentSelected = Math.round((this.clicks / this.views) * 1000) / 10;
 };
+
 /*****End Product Constructor******/
 /**********************************/
 
-//start the selection process on click of the start button (AMAZE ME)
+
+
+//function to start the selection process on click of the start button (AMAZE ME)
 function welcome_start(){
   start.addEventListener('click', initProgram);
 }
 
-
 //function to create objects
 function initProgram(){
-  console.log('click');
   //hide the welcome window
   welcome.classList.add('close');
   //show the selections section
@@ -128,7 +139,7 @@ function initRound(){
   var imgSrc;
   //fetch an array of 3 random indexes
   var randomIndexes = uniqueRandomNumbers(imageCount, 0);
-  //loop through arry of indexes
+  //loop through array of indexes
   for(var i = 0; i < randomIndexes.length; i++){
     //create fetch key using index of key array
     imageKey = productKeys[randomIndexes[i]];
@@ -143,29 +154,6 @@ function initRound(){
   //add event listeners to newly created images
   initListeners();
 }
-
-function goAgain(){
-  //reset images
-  image_display.innerHTML = '';
-  initRound();
-}
-
-//Function to generate three unique numbers and unique from previous round
-//creats array of unique numbers and compares them to previous unique arrray
-function uniqueRandomNumbers(max_num , min_num){
-  var randomNUm;
-  unique_Nums = [];
-
-  while( unique_Nums.length < 3 ){
-    randomNUm = Math.floor((Math.random() * (max_num - min_num)) + min_num);
-    if ( ! unique_Nums.includes(randomNUm) && ! previous_unique.includes(randomNUm)){
-      unique_Nums.push(randomNUm);
-    }
-  }
-  previous_unique = unique_Nums;
-  return unique_Nums;
-}
-
 
 //create evenlisteners
 function initListeners(){
@@ -195,72 +183,68 @@ function registerClick(e){
     }
   }
   //set a deleay before moving on to the next set of choices
-  delayedTime = window.setTimeout(goAgain, timeDelay);
+  window.setTimeout(goAgain, timeDelay);
 }
 
+function goAgain(){
+  //reset images
+  image_display.innerHTML = '';
+  initRound();
+}
+
+//Function to generate three unique numbers and unique from previous round
+//creats array of unique numbers and compares them to previous unique arrray
+function uniqueRandomNumbers(max_num , min_num){
+  var randomNUm;
+  unique_Nums = [];
+
+  while( unique_Nums.length < 3 ){
+    randomNUm = Math.floor((Math.random() * (max_num - min_num)) + min_num);
+    if ( ! unique_Nums.includes(randomNUm) && ! previous_unique.includes(randomNUm)){
+      unique_Nums.push(randomNUm);
+    }
+  }
+  previous_unique = unique_Nums;
+  return unique_Nums;
+}
+
+//function to iniitiate chart.js
 function logResults(){
   //hide selections window
   selections.classList.add('close');
   results_container.classList.remove('close');
   var chartParamiters = new ChartDataSet(allProducts).chartParams;
   chart = new Chart(ctx, chartParamiters);
-  console.log('chartParamiters', chartParamiters);
-  canvas.addEventListener('click', clickHandler);
+  canvas.addEventListener('click', open_reference_image);
 }
 
-function clickHandler(evt) {
+//function to open small product image when clicking on a bar in the chart
+function open_reference_image(evt) {
+  //chart.js api call
   var item = chart.getElementAtEvent(evt)[0];
   if (item) {
     var label = item._view.label;
+    // use the label as the key for a product object
     var item_imagePath = allProducts[label].imagePath;
-    console.log('item_imagePath', item_imagePath);
     displayImage(item_imagePath);
   }
 }
 
+//show the image far a clicked bar
 function displayImage(path){
-  var image_div = document.getElementById('small_image');
+  image_div.innerHTML = '';
   var small_image = document.createElement('img');
   small_image.setAttribute('src', path);
-  small_image.addEventListener('click', function(){image_div.classList.add('close');});
+  small_image.addEventListener('click', function(){
+    image_div.classList.add('close');
+    image_div.innerHTML = '';
+  });
   image_div.appendChild(small_image);
   image_div.classList.remove('close');
 }
 
-/*
-//function to build table of votes
-function logResults(){
-  var newTable = document.createElement('table');
-  var tbody = document.createElement('tbody');
-  for( var i = 0; i < imageCount; i++){
-    imageKey = productKeys[i];
-    tbody.appendChild(allProducts[imageKey].dataRow);
-  }
-  newTable.appendChild(tbody);
-  resultsSection.appendChild(newTable);
-}
-*/
-
-welcome_start()
-console.log('allProducts', allProducts);
-
-console.log('productKeys', productKeys);
-
-
 /**********************************************/
 /**************Chart Constructor *************/
-var chartType = 'bar';
-var percent_background_color = '#C3B867';
-var percent_border_color = '#000000';
-var views_background_color = '#767254';
-var views_border_color = '#767254';
-var clicked_background_color = '#F7D50A';
-var clicked_border_color = '#000000';
-var roundsAxes_labelString = 'Number of Selection Rounds';
-var percentAxes_labelString = 'Percentage of Clicks / View';
-var yAxes_fontSize = 18;
-
-
 function ChartDataSet(dataObject){
   this.product_data_object = dataObject;
   this.type = chartType;
